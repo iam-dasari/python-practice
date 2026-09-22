82a5e70919686dd789c116abdb372fdce51402e9
iam-dasari <dasaridevops2025@gmail.com>
2026-09-22 20:29:20 +0530

    Exception Handling

--- 18-exception-handling.py
+++ 18-exception-handling.py
@@ -1,5 +1,7 @@
 # Exception Handling (try-except)
 # Exception handling is used to handle runtime errors gracefully in Python.
+# else block will be executed if there is no exception in the try block.
+# finally block will always be executed, regardless of whether an exception occurred or not.
 
 try:
     x = int(input("Enter a number: "))
@@ -9,3 +11,7 @@ except ValueError:
     print("Invalid input! Please enter a valid integer.")
 except ZeroDivisionError:
     print("Error! Division by zero is not allowed.")
+else:
+    print("else: No exceptions occurred.")
+finally:
+    print("finally: will always be executed.")
